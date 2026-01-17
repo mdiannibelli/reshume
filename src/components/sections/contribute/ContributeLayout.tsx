c9023aa827ee690f120dfc3ef674b561d1b95178
Marcos Dionel Iannibelli <dioneldeveloper@gmail.com>
2026-01-17 16:56:14 -0300

    style(-): opensource description color & border radious

--- src/components/sections/contribute/ContributeLayout.tsx
+++ src/components/sections/contribute/ContributeLayout.tsx
@@ -7,14 +7,14 @@ export function ContributeLayout() {
   const { t } = useTranslation();
   return (
     <div className="grid grid-cols-2 md:grid-cols-6 gap-x-4">
-      <div className="col-span-2 md:col-span-6 relative inline-block rounded-lg bg-(--primary) backdrop-blur-md border-(--primary-dark) border overflow-hidden group mb-6 rounded-t-4xl">
+      <div className="col-span-2 md:col-span-6 relative inline-block bg-(--primary) backdrop-blur-md border-(--primary-dark) border overflow-hidden group mb-6 rounded-4xl">
         <div className="absolute inset-0 bg-(--primary-darker)/10 translate-y-full group-hover:translate-y-0 transition-transform duration-300"></div>
         <div className="relative flex flex-col h-full p-12">
           <div>
             <h2 className="text-4xl font-medium mb-4 text-(--text-primary)">
               {t("HOME.CONTRIBUTE.LAYOUT.TITLE")}
             </h2>
-            <p className="text-xl text-(--text-secondary) max-w-3xl">
+            <p className="text-base md:text-xl text-(--text-primary) max-w-3xl lg:max-w-4xl">
               {t("HOME.CONTRIBUTE.LAYOUT.DESCRIPTION")}
             </p>
           </div>
@@ -40,7 +40,7 @@ export function ContributeLayout() {
             <h2 className="text-3xl font-medium mb-4 text-(--text-primary)">
               {t("HOME.CONTRIBUTE.CONTRIBUTIONS.TITLE")}
             </h2>
-            <p className="text-lg max-w-2xl text-(--text-secondary) py-2">
+            <p className="text-base md:text-lg max-w-2xl text-(--text-secondary) py-2">
               <Trans
                 i18nKey="HOME.CONTRIBUTE.CONTRIBUTIONS.DESCRIPTION"
                 components={{
@@ -70,7 +70,7 @@ export function ContributeLayout() {
             <h2 className="text-3xl font-medium mb-4 text-(--text-primary)">
               {t("HOME.CONTRIBUTE.RATING.TITLE")}
             </h2>
-            <p className="text-lg max-w-2xl text-(--text-primary) py-2">
+            <p className="text-base md:text-lg max-w-2xl text-(--text-primary) py-2">
               <Trans
                 i18nKey="HOME.CONTRIBUTE.RATING.DESCRIPTION"
                 components={{
